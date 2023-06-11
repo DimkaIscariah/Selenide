e858a66e65fd33299c6a9c658d459df2b9e77b90
Dmitry <once87@yandex.ru>
2023-06-11 23:26:31 +0300

    changes

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -6,11 +6,13 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.files.DownloadActions.click;
+
+
 
 public class CardDeliveryTest {
 
@@ -29,13 +31,15 @@ public class CardDeliveryTest {
         $("[data-test-id=city] input").setValue("Владимир");
         $("[data-test-id=date] input").doubleClick();
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        String currentDate = generateDate(8, "dd.MM.yy");
+        String currentDate = generateDate(5, "dd.MM.yy");
         $("[data-test-id=date] input").setValue(currentDate);
         $("[data-test-id=name] input").setValue("Дмитрий Арабей");
         $("[name='phone']").setValue("+79056173272");
         $("[data-test-id=agreement").click();
         $(byClassName("button")).click();
-        $("[data-test-id=notification").shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + currentDate), Duration.ofSeconds(15)).shouldBe(visible);
-        
+        $("[data-test-id=notification")
+                .shouldHave(Condition.exactText("Успешно! Встреча успешно забронирована на " + currentDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+
     }
 }
