45484a3311c2206ca4878e7c1c06f11eabeba3b2
Dmitry <once87@yandex.ru>
2023-06-11 23:10:46 +0300

    Changes

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,36 +1,41 @@
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-
-import static com.codeborne.selenide.Selenide.*;
-
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class CardDeliveryTest {
 
-    String generateDate(int addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    public String generateDate(int daysToAdd, String pattern) {
+        return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern(pattern));
+
     }
 
 
 
     @Test
-    void shouldCardDelivery() {
-        open ("http://localhost:9999/");
+
+    void shouldCardDeliveryTest() {
+
+        open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Владимир");
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        String currentDate = generateDate(5, "dd.MM.yy");
-        $("[data-test-id=date] input").sendKeys(currentDate);
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        String currentDate = generateDate(8, "dd.MM.yy");
+        $("[data-test-id=date] input").setValue(currentDate);
         $("[data-test-id=name] input").setValue("Дмитрий Арабей");
-        $("[data-test-id=phone] input").setValue("+79056173272");
-        $("[data-test-id=agreement]").click();
-        $$("[type='button']").find(Condition.exactText("Забронировать")).click();
-        $(".notification__content")
-                .shouldHave(Condition.text("Успешно! Встреча забронирована на " + currentDate), Duration.ofSeconds(15))
-                .shouldBe(Condition.visible);
+        $("[name='phone']").setValue("+79056173272");
+        $("[data-test-id=agreement").click();
+        $(byClassName("button")).click();
+        $("[data-test-id=notification").shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + currentDate), Duration.ofSeconds(15)).shouldBe(visible);
+        
     }
 }
